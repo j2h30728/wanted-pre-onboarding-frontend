658fbdc4dc1f34d9259fe234dcdcea06ba886b94
LeeJiHyeon <rachel2148072@gmail.com>
2023-04-08 02:47:03 +0900

    feat:Route Setting

---
+++ src/router.tsx
@@ -0,0 +1,29 @@
+import { Router } from "@remix-run/router";
+import { createBrowserRouter } from "react-router-dom";
+import App from "./App";
+import NotFound from "./pages/NotFound";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Singup";
+import Todo from "./pages/Todo";
+
+const router: Router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <NotFound />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+  {
+    path: "/signin",
+    element: <Signin />,
+  },
+  {
+    path: "/todo",
+    element: <Todo />,
+  },
+]);
+
+export default router;
