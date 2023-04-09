edc95444a027b84ca6de76a4feed8601522af30f
LeeJiHyeon <rachel2148072@gmail.com>
2023-04-09 20:23:37 +0900

    fix:컨테이너 디자인 오류 수정

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar";
 function App() {
   return (
     <div className="w-full h-screen mt-16 flex flex-col justify-start items-center">
-      <div className="w-80 py-3 px-5 flex flex-col items-center bg-zinc-200	border-2 border-zinc-400 border-solid rounded-xl">
+      <div className="w-80 py-3 px-5 h-[600px] flex flex-col items-center bg-zinc-200	border-2 border-zinc-400 border-solid rounded-xl">
         <Link to="/" className="self-start font-pacifico text-m">
           TODO
         </Link>
